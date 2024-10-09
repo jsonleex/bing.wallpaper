22f13ab47effc1fb237089188e0f6f9343d010b9
leex <dev@leex.com>
2024-10-10 02:56:51 +0800

    chore: deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { cwd, env } from 'node:process'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
@@ -11,30 +12,38 @@ export default defineNuxtConfig({
     '@nuxtjs/sitemap',
     'nuxt-simple-robots',
   ],
+
   css: [
     '@unocss/reset/tailwind.css',
     '~/styles/base.css',
   ],
+
   nitro: {
     serverAssets: [{
       baseName: 'archive',
       dir: resolve(cwd(), 'archive'),
     }],
   },
+
   gtag: {
     id: 'G-YR2YHJVK0Q',
   },
+
   site: {
     url: `${env.NUXT_SITE_URL}`,
   },
+
   sitemap: {
     debug: true,
     sources: ['/api/sitemap'],
     defaultSitemapsChunkSize: 1000,
   },
+
   robots: {
     sitemap: [
       `/sitemap.xml`,
     ],
   },
+
+  compatibilityDate: '2024-10-10',
 })
