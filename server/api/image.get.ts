7f7e9113dd85fc521192ec9247e2bdf272a5f108
leex <jsonleex@163.com>
2025-07-28 22:29:23 +0800

    chore: update storage logic

--- server/api/image.get.ts
+++ server/api/image.get.ts
@@ -8,9 +8,15 @@ export default defineEventHandler(
     const query = getQuery<ImageQuery>(event)
 
     const market = useValidMarket(event)
+    const date = new Date(query.date ?? getDateWithMarketOffset(market.offset))
 
-    const key = buildStorageKey(new Date(query.date ?? getDateWithMarketOffset(market.offset)), market.lang)
-    const image = await getCachedImageWithStorageKey(key)
+    const key = [
+      String(date.getFullYear()),
+      String(date.getMonth() + 1).padStart(2, '0'),
+      String(date.getDate()).padStart(2, '0')
+    ].join("")
+
+    const image = await getCachedImageFromStorage(key, market.lang)
 
     if (!image)
       throw createError({ statusCode: 404, message: 'Image not found' })
--- server/utils/storage.ts
+++ server/utils/storage.ts
@@ -1,28 +1,54 @@
 import type { BingImageMeta } from '~/types'
 
-export function buildStorageKey(date: Date, prefix?: string) {
-  const YY = String(date.getFullYear())
-  const MM = String(date.getMonth() + 1).padStart(2, '0')
-  const DD = String(date.getDate()).padStart(2, '0')
+async function getImageGroupFromStorage(group: string, mkt: string) {
+  const storage = useStorage<Record<string, BingImageMeta>>('assets/archive')
 
-  const key = `${YY}${MM}/${YY}${MM}${DD}.json`
-
-  return prefix ? `${prefix}/${key}` : key
+  const groupkey = `${mkt}/${group}.json`
+  if (!await storage.hasItem(groupkey))
+    return null
+  else
+    return await storage.getItem(groupkey)
 }
 
-export async function getImagesFromStorage(idx: number, count: number, market: { lang: string, offset: number }) {
+export const getCachedImageGroupWithStorageKey = cachedFunction(getImageGroupFromStorage, {
+  getKey: (group: string, mkt: string) => `images_${mkt}_${group}`,
+  maxAge: 1000 * 60 * 60 * 8, // 8 Hours
+})
+
+async function getImageFromStorage(date: string, mkt: string) {
+  if (!/\d{8}/.test(date)) {
+    console.warn(`Invalid date: ${date}`)
+    return
+  }
+
+  const group = await getCachedImageGroupWithStorageKey(date.slice(0, 6), mkt)
+
+  return group?.[date]
+}
+
+export const getCachedImageFromStorage = cachedFunction(getImageFromStorage, {
+  getKey: (key: string, mkt: string) => `image_${mkt}_${key}`,
+  maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
+})
+
+async function getImagesFromStorage(idx: number, count: number, market: { lang: string, offset: number }) {
   const images: BingImageMeta[] = []
-  const storage = useStorage<BingImageMeta>('assets/archive')
 
   const today = getDateWithMarketOffset(market.offset)
   const start = new Date(today.setDate(today.getDate() - idx))
   const end = new Date(today.setDate(today.getDate() - count))
 
   while (start.getTime() > end.getTime()) {
-    const key = buildStorageKey(start, market.lang)
+    const key = [
+      String(start.getFullYear()),
+      String(start.getMonth() + 1).padStart(2, '0'),
+      String(start.getDate()).padStart(2, '0')
+    ].join("")
 
-    if (await storage.hasItem(key))
-      images.push((await storage.getItem(key))!)
+    const image = await getCachedImageFromStorage(key, market.lang)
+
+    if (image)
+      images.push(image)
     else
       console.warn(`No image find for ${key}`)
 
@@ -34,20 +60,6 @@ export async function getImagesFromStorage(idx: number, count: number, market: {
 }
 
 export const getCachedImagesFromStorage = cachedFunction(getImagesFromStorage, {
-  getKey: (idx: number, count: number, market: { lang: string, offset: number }) => [market.lang, idx, count].join('_'),
-  maxAge: 1000 * 60 * 60 * 8, // 8 Hours
-})
-
-export async function getImageWithStorageKey(key: string) {
-  const storage = useStorage<BingImageMeta>('assets/archive')
-
-  if (!await storage.hasItem(key))
-    return
-
-  return await storage.getItem(key) as BingImageMeta
-}
-
-export const getCachedImageWithStorageKey = cachedFunction(getImageWithStorageKey, {
-  getKey: (key: string) => key,
-  maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+  getKey: (idx, count, market) => `images_${market.lang}_${idx}_${count}`,
+  maxAge: 1000 * 60 * 60 * 8, // 8 hours
 })
